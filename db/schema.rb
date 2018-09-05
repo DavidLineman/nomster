08bdbfcb194056e43303ef89a86143d9191c3879
David Lineman <dline215@gmail.com>
2018-09-05 19:43:09 +0000

    fixed database error

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,11 @@ ActiveRecord::Schema.define(version: 20180905185814) do
   enable_extension "plpgsql"
 
   create_table "places", force: :cascade do |t|
-    t.text     "name"
+    t.string   "name"
+    t.text     "description"
     t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
