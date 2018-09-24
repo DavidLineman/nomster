fb836e0ff10f4405aa4bc8a1ef96103041a806b7
David Lineman <dline215@gmail.com>
2018-09-24 20:47:29 +0000

    added photo fields

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,15 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    @place = Place.find(params[:place_id])
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption)
+  end
+
+
 end
